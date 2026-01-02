acd8de242953f123feffeda7a9029a160852aaf9
Bakino <contact@bakino.fr>
2026-01-02 21:03:50 +0000

    subroute path

--- server/viewzSsr.mjs
+++ server/viewzSsr.mjs
@@ -109,10 +109,10 @@ export async function generateSsrContent({sourcePath, base, containerId, htmlPro
 
     let allRoutes = [] ;
 
-    function registerRoute({route, parentPath=[], level=0}){
+    function registerRoute({route, parentRoute = "", level=0}){
         let fullRoute = route.url ;
-        if(parentPath){
-          fullRoute = parentPath.filter(p=>p.url !== "/").map(p=>p.url).concat([route.url]).join("/") ;
+        if(parentRoute){
+            fullRoute = parentRoute.replace(/\/$/, "") + "/" + fullRoute.replace(/^\//, "") ;
         }
         let pattern = ((base??"")+fullRoute).replace(/\/:(\w+)\?/g, "{/:$1}")
         route.regexp = pathToRegexp(pattern) ;
@@ -192,7 +192,7 @@ export async function generateSsrContent({sourcePath, base, containerId, htmlPro
 
         if(route.subRoutes){
             for(let subRoute of route.subRoutes){
-                registerRoute({route: subRoute, parentPath: parentPath.concat([route]), level: level+1}) ;
+                registerRoute({route: subRoute, parentRoute: fullRoute, level: level+1}) ;
             }
         }
     }
